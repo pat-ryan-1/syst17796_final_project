730d0c691ab08f7fc3b88b6104085ad428c52e8f
pat-ryan-1 <ryapatri@sheridancollege.ca>
2019-08-11 22:27:15 -0400

    Fix bug in game with multiple application of special cards

--- src/ca/sheridancollege/project/Round.java
+++ src/ca/sheridancollege/project/Round.java
@@ -1,4 +1,3 @@
-
 package ca.sheridancollege.project;
 
 import java.util.ArrayList;
@@ -6,9 +5,9 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
- * This class serves as the main controller of the application. The class retrieves
- * and sends data to and from the user, and interacts with the model classes like
- * deck, hand, player and others.
+ * This class serves as the main controller of the application. The class
+ * retrieves and sends data to and from the user, and interacts with the model
+ * classes like deck, hand, player and others.
  *
  * @author Patrick Ryan and Karam Fasih
  */
@@ -25,16 +24,16 @@ public class Round {
         this.deck = Deck.standardDeckFactory();
         pcp = new PlayedCardPile(new ArrayList<>());
         pcp.add(deck.draw());
-        
+
         // CLear the current hands of the players, if they exist
         p1.getHand().clear();
         p2.getHand().clear();
-        
+
         for (int i = 0; i < 7; ++i) {
             p1.addCardToHand(deck.draw());
             p2.addCardToHand(deck.draw());
         }
-        
+
     }
 
     public Player play() {
@@ -57,7 +56,8 @@ public class Round {
                 }
             }
 
-            while (!playTurn(currentPlayer, selection)) {
+            boolean cardPlayed = playTurn(currentPlayer, selection);
+            while (!cardPlayed) {
                 boolean selectedPrompt = false;
                 int response = 0;
                 while (!selectedPrompt) {
@@ -77,12 +77,13 @@ public class Round {
                         selectedPrompt = true;
                     }
                 }
-                
+
                 if (response == 1) {
                     try {
                         // Get selected card from user
                         selection = getCardSelection(input,
                                 currentPlayer.getHand().getSize());
+                        cardPlayed = playTurn(currentPlayer, selection);
                     } catch (InputMismatchException ex) {
                         System.out.println(ex.getMessage());
                     }
@@ -93,27 +94,29 @@ public class Round {
             }
             // Apply special effects of pickups and skip cards to other player
             Player otherPlayer = currentPlayer.equals(p1) ? p2 : p1;
-            switch (pcp.getTop().getValue()) {
-                case DRAW_TWO:
-                    System.out.println("Oh no! " + otherPlayer.getPlayerID()
-                            + "Has to draw two cards!" );
-                    otherPlayer.addCardToHand(draw());
-                    otherPlayer.addCardToHand(draw());
-                    break;
-                case DRAW_FOUR:
-                    System.out.println("Oh no! " + otherPlayer.getPlayerID()
-                            + "Has to draw four cards!" );
-                    otherPlayer.addCardToHand(draw());
-                    otherPlayer.addCardToHand(draw());
-                    otherPlayer.addCardToHand(draw());
-                    otherPlayer.addCardToHand(draw());
-                    break;
-                case SKIP:
-                    System.out.println("Oh no! " + otherPlayer.getPlayerID()
-                            + "Has been skipped!" );
-                    continue;
-                default:
-                    break;
+            if (cardPlayed) {
+                switch (pcp.getTop().getValue()) {
+                    case DRAW_TWO:
+                        System.out.println("Oh no! " + otherPlayer.getPlayerID()
+                                + "Has to draw two cards!");
+                        otherPlayer.addCardToHand(draw());
+                        otherPlayer.addCardToHand(draw());
+                        break;
+                    case DRAW_FOUR:
+                        System.out.println("Oh no! " + otherPlayer.getPlayerID()
+                                + "Has to draw four cards!");
+                        otherPlayer.addCardToHand(draw());
+                        otherPlayer.addCardToHand(draw());
+                        otherPlayer.addCardToHand(draw());
+                        otherPlayer.addCardToHand(draw());
+                        break;
+                    case SKIP:
+                        System.out.println("Oh no! " + otherPlayer.getPlayerID()
+                                + "Has been skipped!");
+                        continue;
+                    default:
+                        break;
+                }
             }
             currentPlayer = otherPlayer; //switches players
         }
@@ -129,9 +132,9 @@ public class Round {
     }
 
     /**
-     * 
+     *
      * Prompt user for input and then retrieve.
-     * 
+     *
      * @param input scanner for input
      * @param max max value of the integer that can be inputted.
      * @return the number the user selected
@@ -156,13 +159,12 @@ public class Round {
 
     /**
      * Attempt to play a move specified by the user.
-     * 
+     *
      * @param cp current player
      * @param selection the integer id of the selected card
      * @return true if the turn could be played, false otherwise
      */
     public boolean playTurn(Player cp, int selection) {
-        System.out.print(cp.getHand());
         boolean playable = Turn.playable(cp.getCard(selection), pcp.getTop());
         if (playable) {
             Card c = cp.removeCardFromHand(cp.getCard(selection));
@@ -175,9 +177,9 @@ public class Round {
     }
 
     /**
-     * 
+     *
      * Display information for the current player's turn.
-     * 
+     *
      * @param player current player
      */
     public void displayTurnInfo(Player player) {
@@ -194,26 +196,27 @@ public class Round {
         // Print the top of the pile
         System.out.println("Top of discard pile:" + pcp.getTop());
     }
-    
+
     /**
      * Draw a card from the deck. If the deck is empty, reshuffle the played
      * card pile and then draw a card afterwards.
+     *
      * @return a card from the deck.
      */
     public Card draw() {
         if (deck.getSize() == 0) {
-            
+
             // shuffle the discard pile, return it to the deck, then clear the 
             // discard pile.
             System.out.println("Deck out of cards, re-shuffling...");
             pcp.shuffle();
-            deck.setCards((ArrayList<Card>)pcp.getCards().clone());
+            deck.setCards((ArrayList<Card>) pcp.getCards().clone());
             pcp.clear();
-            
+
             // Flip a card for the pile.
             pcp.put(deck.draw());
         }
-        
+
         return deck.draw();
     }
 }
