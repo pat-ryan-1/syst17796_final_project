7c5736cb0de535cf85c5a1bacf506bf263415673
kfasih <k.fasih@outlook.com>
2019-08-12 22:42:15 -0400

    kd push 4

--- test/ca/sheridancollege/project/TurnTest.java
+++ test/ca/sheridancollege/project/TurnTest.java
@@ -1,16 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ca.sheridancollege.project;
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
@@ -18,38 +6,5 @@ import static org.junit.Assert.*;
  */
 public class TurnTest {
     
-    public TurnTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
-    /**
-     * Test of playable method, of class Turn.
-     */
-    @Test
-    public void testPlayable() {
-        System.out.println("playable");
-        Card card = null;
-        Card topCard = null;
-        boolean expResult = false;
-        boolean result = Turn.playable(card, topCard);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
+//All Turn Testing to be done manually.    
 }
